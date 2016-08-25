5ad128512edda22c9cc58ea1a24066035fdeb36c
set2k6 <set2k6@gmail.com>
2016-08-25 16:08:09 -0500

    setting up initial app

--- eventshineSpawnOfNessie/nessie/models.py
+++ eventshineSpawnOfNessie/nessie/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    start_date = models.DateTimeField('date started')
+    end_date = models.DateTimeField('date ended')
+    attendee_limit = models.CharField(max_length=200)
+
+class Venue(models.Model):
+    event = models.ForeignKey(Event, on_delete = models.CASCADE)
+    name = models.CharField(max_length=200)
+    event_limit = models.CharField(max_length=200)
+
+class User(models.Model):
+    event = models.ForeignKey(Event, on_delete = models.CASCADE)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
