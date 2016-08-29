35c8c0a16459164e0dd704c6a39c74b69f40f07f
Keith <keithbradley1@msn.com>
2016-08-29 09:52:36 -0500

    add pycache and views.py changes

--- eventshineSpawnOfNessie/nessie/views.py
+++ eventshineSpawnOfNessie/nessie/views.py
@@ -7,3 +7,26 @@ def index(request):
     # all_events = Event.objects.all()
     return HttpResponse("Hello, world. You're at the nessie event index.")
 
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from .models import Choice, Question# ...
+
+def event():
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
