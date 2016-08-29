ebac07834378d6442272b997861a38c07ebfd75e
Castle Crawford <nathanscottcrawford@gmail.com>
2016-08-29 10:07:10 -0500

    restructured views setup and made index.html

--- eventshineSpawnOfNessie/nessie/urls.py
+++ eventshineSpawnOfNessie/nessie/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
+app_name = 'nessie'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
