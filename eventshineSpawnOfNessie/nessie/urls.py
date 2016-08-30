af1f4d9312a298efe5851554846f4e12fc0b5f26
Keith <keithbradley1@msn.com>
2016-08-30 13:27:55 -0500

    remove the login url

--- eventshineSpawnOfNessie/nessie/urls.py
+++ eventshineSpawnOfNessie/nessie/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
 
     url(r'^register/', views.Register.as_view(), name='register'),
 
+    url(r'^loginUser/', views.loginUser.as_view(), name='loginUser'),
+
+    url(r'^logout/', views.logoutUser.as_view(), name='logout'),
+
+    url(r'^failedLogin/', views.failedLogin.as_view(), name='failedLogin'),
+
     url(r'^register_user/', views.register_user, name='register_user'),
 
     url(r'event/add$', views.EventCreate.as_view(), name='event_add'),
